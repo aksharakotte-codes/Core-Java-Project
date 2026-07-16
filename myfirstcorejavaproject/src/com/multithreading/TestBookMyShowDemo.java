e4d2d146d3516ea4f81ec0c347deed5853555aac
aksharakotte-codes <kotteakshara01@gmail.com>
2026-07-16 10:08:44 +0530

    Committing Book My Show Application using Synchronization on 8-07-2026

---
+++ myfirstcorejavaproject/src/com/multithreading/TestBookMyShowDemo.java
@@ -0,0 +1,60 @@
+package com.multithreading;
+
+class BookMyShow {
+	
+	int total_avaliable_Tickets = 10;
+	
+	synchronized void bookMyTicket(String name, int tickets) {
+		
+		if(total_avaliable_Tickets >= tickets) {
+			System.out.println(tickets + "Tickets has booked Successfully for Ms. " + name);
+			
+			total_avaliable_Tickets = total_avaliable_Tickets - tickets;
+			System.out.println("Remaining Tickets Count is : " + total_avaliable_Tickets);
+		} else {
+			System.err.println("Sorry !! All tickets has been sold out for Ms. " + name);
+			System.err.println("Remaining Tickets Count is : " + total_avaliable_Tickets);
+		}
+		
+	}
+	
+}
+
+class Customer extends Thread{ 
+	
+	BookMyShow bms;
+	String customerName;
+	int tickets;
+	public Customer(BookMyShow bms, String customerName, int tickets) {
+		super();
+		this.bms = bms;
+		this.customerName = customerName;
+		this.tickets = tickets;
+	}
+	
+	@Override
+	public void run() {
+		bms.bookMyTicket(customerName, tickets);
+	}
+	
+}
+public class TestBookMyShowDemo {
+
+
+	public static void main(String[] args) {
+		System.out.println("Main Method Started !!");
+	    BookMyShow bms = new BookMyShow();
+		
+		Customer c1 = new Customer(bms, "Akshara", 5);
+		c1.start();
+		
+		Customer c2 = new Customer(bms, "Vyshanvi", 3);
+		c2.start();
+		
+		Customer c3 = new Customer(bms, "Honey", 3);
+		c3.start();
+		
+		System.out.println("Main Method Ended !!");
+	}
+
+}
