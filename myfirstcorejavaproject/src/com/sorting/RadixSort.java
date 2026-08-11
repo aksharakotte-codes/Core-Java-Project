d4bbe1f347922be2f5644e5097c68adb57f7ff90
aksharakotte-codes <kotteakshara01@gmail.com>
2026-08-11 10:10:47 +0530

    Commitiing DSA Sorting programs on 02-08-2026

---
+++ myfirstcorejavaproject/src/com/sorting/RadixSort.java
@@ -0,0 +1,48 @@
+package com.sorting;
+
+import java.util.Arrays;
+
+public class RadixSort {
+
+    static void countingSort(int[] arr, int exp) {
+
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+
+        for (int i = 0; i < n; i++)
+            count[(arr[i] / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+
+    static void radixSort(int[] arr) {
+
+        int max = arr[0];
+
+        for (int num : arr)
+            if (num > max)
+                max = num;
+
+        for (int exp = 1; max / exp > 0; exp *= 10)
+            countingSort(arr, exp);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+
+        radixSort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
